57efcfd2829e3ef087f3f3474809fba2b88f5bc7
Nosilla973 <98498484+Nosilla973@users.noreply.github.com>
2022-02-02 16:17:46 -0500

    Update Programa.py

--- Programa.py
+++ Programa.py
@@ -1,4 +1,4 @@
-Lista_Jugueria = ['manzanos', 'peros', 'platanos', 'fresas', 'leche', 'yogurt', 'cereales', 'papaya']
+Lista_Jugueria = ['manzanas', 'peras', 'platano', 'fresa', 'leche', 'yogurt', 'cereales', 'papaya']
 Dias = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo']
 
 
